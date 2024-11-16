c833e325ef3e2cf047da28e742170bb3bf912ac0
Nikita Lolenko <loleenko@vk.com>
2024-11-16 16:41:16 +0300

    Рефакторинг функций генетического алгоритма и добавление тестирования с предопределенными данными

--- 3task.py
+++ 3task.py
@@ -6,7 +6,7 @@ import math
 def binary_to_int(binary_str):
     return int(binary_str, 2)
 
-# Обновленная функция приспособленности для пользовательских функций
+# Функция приспособленности
 def fitness(individual, func):
     x = binary_to_int(individual)
     return func(x)
@@ -43,17 +43,15 @@ def select_elite(population, func):
     elite = sorted_population[0]
     return elite, sorted_population[1:]
 
-# Обновленная функция кроссовера с возможностью задания точки кроссовера
+# Функция кроссовера
 def crossover(parent1, parent2, test_point=None):
     if test_point is not None:
         point = test_point
         offspring1 = parent1[:point] + parent2[point:]
         offspring2 = parent2[:point] + parent1[point:]
     else:
-        # Поиск возможных точек кроссовера, где потомки будут отличаться от родителей
         possible_points = [i for i in range(1, len(parent1)) if parent1[i:] != parent2[i:]]
         if not possible_points:
-            # Нет точек кроссовера, приводящих к изменению потомков
             point = None
             offspring1 = parent1
             offspring2 = parent2
@@ -70,7 +68,7 @@ def display_crossover(parent, point):
     else:
         return parent[:point] + '*' + parent[point:]
 
-# Обновленная функция мутации с возможностью задания индекса бита для мутации
+# Функция мутации
 def mutate(individual, test_index=None):
     if test_index is not None:
         index = test_index
@@ -80,7 +78,123 @@ def mutate(individual, test_index=None):
     mutated[index] = '1' if mutated[index] == '0' else '0'
     return ''.join(mutated), index
 
-# Основная функция генетического алгоритма
+# Основная функция тестирования генетического алгоритма
+def test_genetic_algorithm():
+    print("Запуск тестирования генетического алгоритма с предопределенными данными.\n")
+    
+    # Предопределенные данные
+    initial_population = ["00100", "01000", "01000", "00001"]
+    generations = 1
+    x_min = 0
+    x_max = 31
+    population_size = 4
+    use_elitism = True
+    
+    # Функция приспособленности
+    func = lambda x: (2 * x + 3) ** 2
+    
+    population = initial_population.copy()
+    print(f"Начальная популяция: {population}")
+    
+    for gen in range(1, generations + 1):
+        fitness_values = [fitness(ind, func) for ind in population]
+        print(f"\nПоколение {gen}:")
+        table = []
+        for idx, ind in enumerate(population):
+            x_value = binary_to_int(ind)
+            table.append([idx + 1, ind, x_value, fitness_values[idx]])
+        print(tabulate(table, headers=["№", "Генотип", "Фенотип (x)", "Значение функции"], tablefmt="pretty"))
+        
+        if use_elitism:
+            elite, others = select_elite(population, func)
+            parent_pool = others
+        else:
+            elite = None
+            parent_pool = population[:]
+        
+        # Кроссовер между Родителем 1 и Родителем 2
+        parent1 = "00100"
+        parent2 = "01000"
+        test_point1 = 1
+        
+        offspring1, offspring2, point1 = crossover(parent1, parent2, test_point1)
+        
+        # Кроссовер между Родителем 3 и Родителем 4
+        parent3 = "01000"
+        parent4 = "00001"
+        test_point2 = 3
+        
+        offspring3, offspring4, point2 = crossover(parent3, parent4, test_point2)
+        
+        # Мутация Потомка 4
+        mutated_offspring4, index = mutate(offspring4, test_index=4)
+        
+        # Сводная таблица после кроссовера и мутации
+        print("\nСводная таблица после кроссовера и мутации:")
+        crossover_table = []
+        parent_offspring_pairs = [
+            ("00100", offspring1),
+            ("01000", offspring2),
+            ("01000", offspring3),
+            ("00001", mutated_offspring4),
+        ]
+
+        for parent, offspring in parent_offspring_pairs:
+            fitness_value = fitness(offspring, func)
+            crossover_table.append([parent, offspring, fitness_value])
+
+        print(tabulate(crossover_table, headers=["Родитель", "Потомок", "Значение функции"], tablefmt="pretty"))
+        
+        # Собираем всех потомков
+        offspring_list = [offspring1, offspring2, offspring3, mutated_offspring4]
+        
+        # Отбор лучших особей
+        combined_population = population + offspring_list
+        sorted_population = sorted(combined_population, key=lambda ind: fitness(ind, func), reverse=True)
+        best_individuals = sorted_population[:population_size]
+        
+        population = best_individuals  # Обновляем популяцию для следующего поколения
+        
+        # Итоговая таблица с указанием ненужных особей
+        print("\nИтоговая таблица родителей и значений:")
+        final_individuals = [
+            ("00100", fitness("00100", func)),
+            ("01000", fitness("01000", func)),
+            ("00001", fitness("00001", func)),
+            ("01000", fitness("01000", func)),
+            ("00100", fitness("00100", func)),
+            ("01001", fitness("01001", func)),
+            ("00000", fitness("00000", func)),
+            ("01000", fitness("01000", func)),
+        ]
+
+        best_individuals_list = population.copy()
+
+        final_table = []
+        for ind, val in final_individuals:
+            if ind in best_individuals_list:
+                status = ""
+                best_individuals_list.remove(ind)
+            else:
+                status = "Зачеркнут"
+            final_table.append([ind, val, status])
+
+        print(tabulate(final_table, headers=["Индивид", "Значение функции", "Статус"], tablefmt="pretty"))
+        
+    # Итоговый результат
+    best_individual = population[0]
+    best_fitness = fitness(best_individual, func)
+    best_x = binary_to_int(best_individual)
+    print("\nФинальное поколение:")
+    print(f"Лучшее значение функции: f({best_x}) = {best_fitness} при генотипе '{best_individual}'")
+    
+    # Проверка ожидаемого результата
+    expected_best_individual = "01001"
+    expected_best_fitness = 441
+    assert best_individual == expected_best_individual, f"Ожидалось: {expected_best_individual}, Получено: {best_individual}"
+    assert best_fitness == expected_best_fitness, f"Ожидалось: {expected_best_fitness}, Получено: {best_fitness}"
+    print("\nТестирование прошло успешно. Результаты совпадают с ожидаемыми.\n")
+
 def genetic_algorithm():
     func = input_function()
     print("Введите диапазон значений x:")
@@ -163,122 +277,11 @@ def genetic_algorithm():
     print("\nФинальное поколение:")
     print(f"Лучшее значение функции: f({best_x}) = {best_fitness} при генотипе '{best_individual}'")
 
-# Обновленная функция тестирования генетического алгоритма с предопределенными данными
-def test_genetic_algorithm():
-    print("Запуск тестирования генетического алгоритма с предопределенными данными.\n")
-    
-    # Предопределенные данные, соответствующие ручному решению
-    initial_population = ["00100", "01000", "01000", "00001"]
-    generations = 1  # Тестируем только одно поколение
-    x_min = 0
-    x_max = 31  # 5-битовые числа от 0 до 31
-    population_size = 4
-    use_elitism = True
-    
-    # Функция приспособленности
-    func = lambda x: (2 * x + 3) ** 2
-    
-    population = initial_population.copy()
-    print(f"Начальная популяция: {population}")
-    
-    for gen in range(1, generations + 1):
-        fitness_values = [fitness(ind, func) for ind in population]
-        print(f"\nПоколение {gen}:")
-        table = []
-        for idx, ind in enumerate(population):
-            x_value = binary_to_int(ind)
-            table.append([idx + 1, ind, x_value, fitness_values[idx]])
-        print(tabulate(table, headers=["№", "Генотип", "Фенотип (x)", "Значение функции"], tablefmt="pretty"))
-        
-        if use_elitism:
-            elite, others = select_elite(population, func)
-            parent_pool = others
-        else:
-            elite = None
-            parent_pool = population[:]
-        
-        # Кроссовер между Родителем 1 и Родителем 2
-        parent1 = "00100"
-        parent2 = "01000"
-        test_point1 = 1  # Точка кроссовера после первого бита
-        
-        offspring1, offspring2, point = crossover(parent1, parent2, test_point1)
-        
-        print("\nКроссовер между Родителем 1 и Родителем 2:")
-        parent1_display = display_crossover(parent1, point)
-        parent2_display = display_crossover(parent2, point)
-        print(f"Родитель 1: '{parent1_display}'")
-        print(f"Родитель 2: '{parent2_display}'")
-        print(f"Точка кроссовера: {point}")
-        print(f"Потомок 1: '{offspring1}'")
-        print(f"Потомок 2: '{offspring2}'")
-        
-        # Кроссовер между Родителем 3 и Родителем 4
-        parent3 = "01000"
-        parent4 = "00001"
-        test_point2 = 3  # Точка кроссовера после третьего бита
-        
-        offspring3, offspring4, point = crossover(parent3, parent4, test_point2)
-        
-        print("\nКроссовер между Родителем 3 и Родителем 4:")
-        parent3_display = display_crossover(parent3, point)
-        parent4_display = display_crossover(parent4, point)
-        print(f"Родитель 3: '{parent3_display}'")
-        print(f"Родитель 4: '{parent4_display}'")
-        print(f"Точка кроссовера: {point}")
-        print(f"Потомок 3: '{offspring3}'")
-        print(f"Потомок 4: '{offspring4}'")
-        
-        # Мутация Потомка 4
-        mutated_offspring4, index = mutate(offspring4, test_index=4)  # Мутация последнего бита
-        print(f"\nМутация Потомка 4:")
-        print(f"Индивид до мутации: '{offspring4}'")
-        print(f"Индивид после мутации: '{mutated_offspring4}' (бит {index})")
-        
-        # Собираем всех потомков
-        offspring_list = [offspring1, offspring2, offspring3, mutated_offspring4]
-        
-        # Вычисляем значения функции для потомков
-        print("\nВычисление значений функции для потомков:")
-        offspring_table = []
-        for idx, ind in enumerate(offspring_list):
-            x_value = binary_to_int(ind)
-            fitness_value = fitness(ind, func)
-            offspring_table.append([idx + 1, ind, x_value, fitness_value])
-        print(tabulate(offspring_table, headers=["№", "Генотип", "Фенотип (x)", "Значение функции"], tablefmt="pretty"))
-        
-        # Отбор лучших особей
-        combined_population = offspring_list
-        sorted_population = sorted(combined_population, key=lambda ind: fitness(ind, func), reverse=True)
-        best_individuals = sorted_population[:population_size]
-        
-        population = best_individuals  # Обновляем популяцию для следующего поколения
-        
-        print("\nОтобранные особи для следующего поколения:")
-        selection_table = []
-        for idx, ind in enumerate(population):
-            x_value = binary_to_int(ind)
-            fitness_value = fitness(ind, func)
-            selection_table.append([idx + 1, ind, x_value, fitness_value])
-        print(tabulate(selection_table, headers=["№", "Генотип", "Фенотип (x)", "Значение функции"], tablefmt="pretty"))
-        
-    # Итоговый результат
-    best_individual = population[0]
-    best_fitness = fitness(best_individual, func)
-    best_x = binary_to_int(best_individual)
-    print("\nФинальное поколение:")
-    print(f"Лучшее значение функции: f({best_x}) = {best_fitness} при генотипе '{best_individual}'")
-    
-    # Ожидаемый результат
-    expected_best_individual = "01001"
-    expected_best_fitness = 441
-    assert best_individual == expected_best_individual, f"Ожидалось: {expected_best_individual}, Получено: {best_individual}"
-    assert best_fitness == expected_best_fitness, f"Ожидалось: {expected_best_fitness}, Получено: {best_fitness}"
-    print("\nТестирование прошло успешно. Результаты совпадают с ожидаемыми.\n")
-
 if __name__ == "__main__":
-    choice = input("Вы хотите запустить генетический алгоритм (1) или тестирование (2)? Введите '1' или '2': ").strip()
-    if choice == '2':
+    choice = input("Введите '1' для запуска тестирования или '2' для запуска генетического алгоритма: ").strip()
+    if choice == '1':
         test_genetic_algorithm()
-    else:
+    elif choice == '2':
         genetic_algorithm()
+    else:
+        print("Неверный выбор. Завершение программы.")
